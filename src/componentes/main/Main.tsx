bc4fe8e99a2d8b4380247fa831dcbde02f63a3c2
thais0 <thais.santos13@estudante.ifms.edu.br>
2023-09-12 10:14:10 -0400

    atv12/09/2023

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,13 +1,56 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
+type FilmeType = {
+       id:number,
+       titulo:string,
+       sinopse:string,
+       imagem:string
+   }
 
 export default function Main(){
+       //let texto = 'Barbie'
+       //Hooks são funções do React que ajudam a gente a fazer tarefas específicas
+       const [texto,setTexto]=useState("Barbie")
+       //O parâmetro "e" da minha função será o meu evento que ocorreu
+
+       const filmes:FilmeType[] = [
+              {
+                   id:1,
+                   titulo:'Barbie',
+                   sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                   imagem:'/barbie.png'
+              },
+              {  
+                   id:2,
+                   titulo:'Barbie',
+                   sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                   imagem:'/ken.png'
+              },
+              {
+                   id:3,
+                   titulo:'Barbie',
+                   sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                   imagem:'/boneca-linda-garota-gerada-por-ai.jpg'
+              }
+       ]
+
+       function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+              //console.log(e.target.value)
+              //Como eu faço para mudar o texto para "TERE"
+              setTexto (e.target.value)
+       }
     return(
         <>
         <div className='campo_pesquisa'>
-            <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme'/>
-            <p>Digitado</p>
+            <p>Busque um filme</p>
+            <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange={TrataTexto}/>
+            {
+              (texto)?
+              <p>Resultados Para: {texto}</p>:""
+            }
         </div>
+        {/** */}
         <main className="content-main">
             <Filme titulo='Barbie'
                    sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
